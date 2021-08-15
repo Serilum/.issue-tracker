b7f0e55150a829c0148ad9768cd5e456c11e9259
Rick South <38502394+ricksouth@users.noreply.github.com>
2021-08-16 01:09:10 +0200

    Added Compact Help Command

---
+++ sources/Compact Help Command/src/main/java/com/natamus/compacthelpcommand/Main.java
@@ -0,0 +1,48 @@
+/*
+ * This is the latest source code of Compact Help Command.
+ * Minecraft version: 1.17.1, mod version: 1.0.
+ *
+ * If you'd like access to the source code of previous Minecraft versions or previous mod versions, consider becoming a Github Sponsor or Patron.
+ * You'll be added to a private repository which contains all versions' source of Compact Help Command ever released, along with some other perks.
+ *
+ * Github Sponsor link: https://github.com/sponsors/ricksouth
+ * Patreon link: https://patreon.com/ricksouth
+ *
+ * Becoming a Sponsor or Patron allows me to dedicate more time to the development of mods.
+ * Thanks for looking at the source code! Hope it's of some use to your project. Happy modding!
+ */
+
+package com.natamus.compacthelpcommand;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.compacthelpcommand.cmds.CommandHelp;
+import com.natamus.compacthelpcommand.config.ConfigHandler;
+import com.natamus.compacthelpcommand.util.Reference;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+
+@Mod(Reference.MOD_ID)
+public class Main {
+	public static Main instance;
+	
+    public Main() {
+        instance = this;
+        
+        ModLoadingContext modLoadingContext = ModLoadingContext.get();
+    	
+        MinecraftForge.EVENT_BUS.register(this);
+        modLoadingContext.registerConfig(ModConfig.Type.COMMON, ConfigHandler.spec);
+
+        RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+    }
+
+    @SubscribeEvent
+    public void registerCommands(RegisterCommandsEvent e) {
+    	CommandHelp.register(e.getDispatcher());
+    }
+}
---
+++ sources/Compact Help Command/src/main/java/com/natamus/compacthelpcommand/config/ConfigHandler.java
@@ -0,0 +1,46 @@
+/*
+ * This is the latest source code of Compact Help Command.
+ * Minecraft version: 1.17.1, mod version: 1.0.
+ *
+ * If you'd like access to the source code of previous Minecraft versions or previous mod versions, consider becoming a Github Sponsor or Patron.
+ * You'll be added to a private repository which contains all versions' source of Compact Help Command ever released, along with some other perks.
+ *
+ * Github Sponsor link: https://github.com/sponsors/ricksouth
+ * Patreon link: https://patreon.com/ricksouth
+ *
+ * Becoming a Sponsor or Patron allows me to dedicate more time to the development of mods.
+ * Thanks for looking at the source code! Hope it's of some use to your project. Happy modding!
+ */
+
+package com.natamus.compacthelpcommand.config;
+
+import net.minecraftforge.common.ForgeConfigSpec;
+
+public class ConfigHandler {
+	private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
+	public static final General GENERAL = new General(BUILDER);
+	public static final ForgeConfigSpec spec = BUILDER.build();
+
+	public static class General {
+		public final ForgeConfigSpec.ConfigValue<Integer> amountCommandsPerPage;
+		
+		public final ForgeConfigSpec.ConfigValue<Integer> commandColour;
+		public final ForgeConfigSpec.ConfigValue<Integer> subcommandColour;
+
+		public General(ForgeConfigSpec.Builder builder) {
+			builder.push("General");
+			amountCommandsPerPage = builder
+					.comment("The message which will be sent to players when they use the /vote command.")
+					.defineInRange("amountCommandsPerPage", 8, 1, 50);
+			
+			commandColour = builder
+					.comment("The colour of the command in /help. The possible values are; 0: black, 1: dark_blue, 2: dark_green, 3: dark_aqua, 4: dark_red, 5: dark_purple, 6: gold, 7: gray, 8: dark_gray, 9: blue, 10: green, 11: aqua, 12: red, 13: light_purple, 14: yellow, 15: white.")
+					.defineInRange("commandColour", 2, 0, 15);
+			subcommandColour = builder
+					.comment("The colour of the subcommand in /help. The possible values are; 0: black, 1: dark_blue, 2: dark_green, 3: dark_aqua, 4: dark_red, 5: dark_purple, 6: gold, 7: gray, 8: dark_gray, 9: blue, 10: green, 11: aqua, 12: red, 13: light_purple, 14: yellow, 15: white.")
+					.defineInRange("subcommandColour", 7, 0, 15);
+			
+			builder.pop();
+		}
+	}
+}
