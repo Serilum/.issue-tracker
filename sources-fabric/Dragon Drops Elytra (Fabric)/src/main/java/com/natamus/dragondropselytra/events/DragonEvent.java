af57b831a4e7fd1bb80035daab7710b034795c6a
Rick South <38502394+ricksouth@users.noreply.github.com>
2021-09-23 15:32:05 +0200

    Updated mod sources.

---
+++ sources-fabric/Dragon Drops Elytra (Fabric)/src/main/java/com/natamus/dragondropselytra/events/DragonEvent.java
@@ -0,0 +1,75 @@
+/*
+ * This is the latest source code of Dragon Drops Elytra.
+ * Minecraft version: 1.17.x, mod version: 1.4.
+ *
+ * If you'd like access to the source code of previous Minecraft versions or previous mod versions, consider becoming a Github Sponsor or Patron.
+ * You'll be added to a private repository which contains all versions' source of Dragon Drops Elytra ever released, along with some other perks.
+ *
+ * Github Sponsor link: https://github.com/sponsors/ricksouth
+ * Patreon link: https://patreon.com/ricksouth
+ *
+ * Becoming a Sponsor or Patron allows me to dedicate more time to the development of mods.
+ * Thanks for looking at the source code! Hope it's of some use to your project. Happy modding!
+ */
+
+package com.natamus.dragondropselytra.events;
+
+import java.util.List;
+
+import com.natamus.collective_fabric.functions.StringFunctions;
+
+import net.minecraft.ChatFormatting;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.boss.enderdragon.EnderDragon;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.AABB;
+
+public class DragonEvent {
+	public static void mobItemDrop(Level world, Entity entity, DamageSource damageSource) {
+		if (world.isClientSide) {
+			return;
+		}
+		
+		if (entity instanceof EnderDragon == false) {
+			return;
+		}
+		
+		Player player = null;
+		BlockPos epos = entity.blockPosition();
+		Entity source = damageSource.getEntity();
+		
+		if (source == null) {
+			List<Entity> entitiesaround = world.getEntities(entity, new AABB(epos.getX()-30, epos.getY()-30, epos.getZ()-30, epos.getX()+30, epos.getY()+30, epos.getZ()+30));
+			for (Entity ea : entitiesaround) {
+				if (ea instanceof Player) {
+					player = (Player)ea;
+					break;
+				}
+			}
+		}
+		else if (source instanceof Player) {
+			player = (Player)source;
+		}
+		
+		StringFunctions.broadcastMessage(world, "It seems like the slain Ender Dragon dropped an elytra! Perhaps it previously belonged to another adventurer?", ChatFormatting.DARK_GREEN);
+		
+		ItemStack elytrastack = new ItemStack(Items.ELYTRA, 1);
+		if (player == null) {
+			ItemEntity elytra = new ItemEntity(world, epos.getX(), epos.getY()+1, epos.getZ(), elytrastack);
+			world.addFreshEntity(elytra);
+		}
+		else {
+			BlockPos pos = player.blockPosition();
+			ItemEntity elytra = new ItemEntity(world, pos.getX(), pos.getY()+1, pos.getZ(), elytrastack);
+			world.addFreshEntity(elytra);
+			
+			StringFunctions.sendMessage(player, "The elytra dropped at your position!", ChatFormatting.YELLOW);
+		}
+	}
+}
