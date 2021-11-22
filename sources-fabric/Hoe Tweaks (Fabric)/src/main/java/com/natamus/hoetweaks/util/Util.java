166b54022600161cd534260aa53dba4ab1302af9
Rick South <38502394+ricksouth@users.noreply.github.com>
2021-11-22 23:33:11 +0100

    Updated mods.

--- sources-fabric/Hoe Tweaks (Fabric)/src/main/java/com/natamus/hoetweaks/util/Util.java
+++ sources-fabric/Hoe Tweaks (Fabric)/src/main/java/com/natamus/hoetweaks/util/Util.java
@@ -1,6 +1,6 @@
 /*
  * This is the latest source code of Hoe Tweaks.
- * Minecraft version: 1.17.x, mod version: 1.0.
+ * Minecraft version: 1.17.x, mod version: 1.1.
  *
  * If you'd like access to the source code of previous Minecraft versions or previous mod versions, consider becoming a Github Sponsor or Patron.
  * You'll be added to a private repository which contains all versions' source of Hoe Tweaks ever released, along with some other perks.
@@ -34,6 +34,11 @@ public class Util {
 	public static int getHoeRange(ItemStack stack) {
 		HoeItem item = (HoeItem)stack.getItem();
 		Tier tier = item.getTier();
+		if (tier == null) {
+			return ConfigHandler.woodenTierHoeRange.getValue();
+		}
+		
+		String tierstring = tier.toString().toLowerCase();
 		
 		int range = ConfigHandler.woodenTierHoeRange.getValue();
 		if (tier.equals(Tiers.STONE)) {
@@ -42,7 +47,7 @@ public class Util {
 		else if (tier.equals(Tiers.GOLD)) {
 			range = ConfigHandler.goldTierHoeRange.getValue();
 		}
-		else if (tier.equals(Tiers.IRON)) {
+		else if (tier.equals(Tiers.IRON) || tierstring.equals("steel")) {
 			range = ConfigHandler.ironTierHoeRange.getValue();
 		}
 		else if (tier.equals(Tiers.DIAMOND)) {
--- sources/Hoe Tweaks/src/main/java/com/natamus/hoetweaks/util/Reference.java
+++ sources/Hoe Tweaks/src/main/java/com/natamus/hoetweaks/util/Reference.java
@@ -1,6 +1,6 @@
 /*
  * This is the latest source code of Hoe Tweaks.
- * Minecraft version: 1.17.1, mod version: 1.0.
+ * Minecraft version: 1.17.1, mod version: 1.1.
  *
  * If you'd like access to the source code of previous Minecraft versions or previous mod versions, consider becoming a Github Sponsor or Patron.
  * You'll be added to a private repository which contains all versions' source of Hoe Tweaks ever released, along with some other perks.
@@ -17,6 +17,6 @@ package com.natamus.hoetweaks.util;
 public class Reference {
 	public static final String MOD_ID = "hoetweaks";
 	public static final String NAME = "Hoe Tweaks";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.1";
 	public static final String ACCEPTED_VERSIONS = "[1.17.1]";
 }
