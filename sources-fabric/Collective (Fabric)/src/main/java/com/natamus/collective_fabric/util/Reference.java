8c9b3f9ceb61b04c470063923a12d242901e7572
Rick South <rick@natamus.com>
2022-02-05 18:45:34 +0100

    Updated mods

--- sources-fabric/Collective (Fabric)/src/main/java/com/natamus/collective_fabric/util/Reference.java
+++ sources-fabric/Collective (Fabric)/src/main/java/com/natamus/collective_fabric/util/Reference.java
@@ -1,6 +1,6 @@
 /*
  * This is the latest source code of Collective.
- * Minecraft version: 1.18.x, mod version: 4.0.
+ * Minecraft version: 1.18.x, mod version: 4.1.
  *
  * If you'd like access to the source code of previous Minecraft versions or previous mod versions, consider becoming a Github Sponsor or Patron.
  * You'll be added to a private repository which contains all versions' source of Collective ever released, along with some other perks.
@@ -17,6 +17,6 @@ package com.natamus.collective_fabric.util;
 public class Reference {
 	public static final String MOD_ID = "collective-fabric";
 	public static final String NAME = "Collective (Fabric)";
-	public static final String VERSION = "4.0";
+	public static final String VERSION = "4.1";
 	public static final String ACCEPTED_VERSIONS = "[1.18.x]";
 }
