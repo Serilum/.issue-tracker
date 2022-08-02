70c15cae2dab0de690c79cb416990a18e14d2e6c
Rick South <rick@natamus.com>
2022-08-02 20:41:54 +0200

    Updated mods.

--- sources/Collective/src/main/java/com/natamus/collective/functions/FABFunctions.java
+++ sources/Collective/src/main/java/com/natamus/collective/functions/FABFunctions.java
@@ -1,6 +1,6 @@
 /*
  * This is the latest source code of Collective.
- * Minecraft version: 1.19.1, mod version: 4.30.
+ * Minecraft version: 1.19.1, mod version: 4.37.
  *
  * Please don't distribute without permission.
  * For all modding projects, feel free to visit the CurseForge page: https://curseforge.com/members/serilum/projects
