cc5d66291b8aa18906c4042c6715d16dd141259f
Rick South <rick@natamus.com>
2022-09-14 14:33:25 +0200

    Updated mod source format

--- sources-fabric/Configurable Mob Potion Effects (Fabric)/src/main/java/com/natamus/configurablemobpotioneffects/Main.java
+++ sources-fabric/Configurable Mob Potion Effects (Fabric)/src/main/java/com/natamus/configurablemobpotioneffects/Main.java
@@ -6,6 +6,12 @@
  * For all Minecraft modding projects, feel free to visit my profile page on CurseForge or Modrinth.
  *  CurseForge: https://curseforge.com/members/serilum/projects
  *  Modrinth: https://modrinth.com/user/serilum
+ *  Overview: https://serilum.com/
+ *
+ * If you are feeling generous and would like to support the development of the mods, you can!
+ *  https://ricksouth.com/donate contains all the information. <3
+ *
+ * Thanks for looking at the source code! Hope it's of some use to your project. Happy modding!
  */
 
 package com.natamus.configurablemobpotioneffects;
