940dfb4eebbfa75f6ab5501edf9d55fb1e106bbf
Rick South <rick@natamus.com>
2022-09-27 13:31:45 +0200

    Updated mods

---
+++ sources-fabric/Nutritious Milk (Fabric)/src/main/java/com/natamus/nutritiousmilk/Main.java
@@ -0,0 +1,42 @@
+/*
+ * This is the latest source code of Nutritious Milk.
+ * Minecraft version: 1.19.2, mod version: 2.1.
+ *
+ * Please don't distribute without permission.
+ * For all Minecraft modding projects, feel free to visit my profile page on CurseForge or Modrinth.
+ *  CurseForge: https://curseforge.com/members/serilum/projects
+ *  Modrinth: https://modrinth.com/user/serilum
+ *  Overview: https://serilum.com/
+ *
+ * If you are feeling generous and would like to support the development of the mods, you can!
+ *  https://ricksouth.com/donate contains all the information. <3
+ *
+ * Thanks for looking at the source code! Hope it's of some use to your project. Happy modding!
+ */
+
+package com.natamus.nutritiousmilk;
+
+import com.natamus.collective_fabric.check.RegisterMod;
+import com.natamus.collective_fabric.fabric.callbacks.CollectiveItemEvents;
+import com.natamus.nutritiousmilk.config.ConfigHandler;
+import com.natamus.nutritiousmilk.events.MilkEvent;
+import com.natamus.nutritiousmilk.util.Reference;
+import net.fabricmc.api.ModInitializer;
+
+public class Main implements ModInitializer {
+	@Override
+	public void onInitialize() { 
+		ConfigHandler.setup();
+
+		registerEvents();
+		
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+	
+	private void registerEvents() {
+		CollectiveItemEvents.ON_ITEM_USE_FINISHED.register((player, usedItem, newItem, hand) -> {
+			MilkEvent.onDrink(player, usedItem, newItem, hand);
+			return null;
+		});
+	}
+}
---
+++ sources-fabric/Nutritious Milk (Fabric)/src/main/java/com/natamus/nutritiousmilk/config/ConfigHandler.java
@@ -0,0 +1,71 @@
+/*
+ * This is the latest source code of Nutritious Milk.
+ * Minecraft version: 1.19.2, mod version: 2.1.
+ *
+ * Please don't distribute without permission.
+ * For all Minecraft modding projects, feel free to visit my profile page on CurseForge or Modrinth.
+ *  CurseForge: https://curseforge.com/members/serilum/projects
+ *  Modrinth: https://modrinth.com/user/serilum
+ *  Overview: https://serilum.com/
+ *
+ * If you are feeling generous and would like to support the development of the mods, you can!
+ *  https://ricksouth.com/donate contains all the information. <3
+ *
+ * Thanks for looking at the source code! Hope it's of some use to your project. Happy modding!
+ */
+
+package com.natamus.nutritiousmilk.config;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+import com.natamus.nutritiousmilk.util.Reference;
+
+import io.github.fablabsmc.fablabs.api.fiber.v1.exception.ValueDeserializationException;
+import io.github.fablabsmc.fablabs.api.fiber.v1.schema.type.derived.ConfigTypes;
+import io.github.fablabsmc.fablabs.api.fiber.v1.serialization.FiberSerialization;
+import io.github.fablabsmc.fablabs.api.fiber.v1.serialization.JanksonValueSerializer;
+import io.github.fablabsmc.fablabs.api.fiber.v1.tree.ConfigTree;
+import io.github.fablabsmc.fablabs.api.fiber.v1.tree.PropertyMirror;
+
+public class ConfigHandler { 
+	public static PropertyMirror<Integer> hungerLevelIncrease = PropertyMirror.create(ConfigTypes.INTEGER);
+	public static PropertyMirror<Double> saturationLevelIncrease = PropertyMirror.create(ConfigTypes.DOUBLE);
+
+	private static final ConfigTree CONFIG = ConfigTree.builder() 
+			.beginValue("hungerLevelIncrease", ConfigTypes.INTEGER, 10)
+			.withComment("The hunger level decreased. Example values: cookie = 2, bread = 5, salmon = 6, steak = 8.")
+			.finishValue(hungerLevelIncrease::mirror)
+
+			.beginValue("saturationLevelIncrease", ConfigTypes.DOUBLE, 10.0)
+			.withComment("The saturation increase. Example values: melon = 1.2, carrot = 3.6, chicken = 7.2, steak 12.8.")
+			.finishValue(saturationLevelIncrease::mirror)
+
+			.build();
+
+	private static void writeDefaultConfig(Path path, JanksonValueSerializer serializer) {
+		try (OutputStream s = new BufferedOutputStream(Files.newOutputStream(path, StandardOpenOption.WRITE, StandardOpenOption.CREATE_NEW))) {
+			FiberSerialization.serialize(CONFIG, s, serializer);
+		} catch (IOException ignored) {}
+
+	}
+
+	public static void setup() {
+		JanksonValueSerializer serializer = new JanksonValueSerializer(false);
+		Path p = Paths.get("config", Reference.MOD_ID + ".json");
+		writeDefaultConfig(p, serializer);
+
+		try (InputStream s = new BufferedInputStream(Files.newInputStream(p, StandardOpenOption.READ, StandardOpenOption.CREATE))) {
+			FiberSerialization.deserialize(CONFIG, s, serializer);
+		} catch (IOException | ValueDeserializationException e) {
+			System.out.println("Error loading config");
+		}
+	}
+}
