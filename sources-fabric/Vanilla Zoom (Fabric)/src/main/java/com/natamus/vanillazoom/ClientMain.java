bb240b688b68be7a4b919cdbf00e99a779bc0dea
Rick South <rick@natamus.com>
2022-10-30 22:33:33 +0100

    Updated mods

---
+++ sources-fabric/Vanilla Zoom (Fabric)/src/main/java/com/natamus/vanillazoom/ClientMain.java
@@ -0,0 +1,50 @@
+/*
+ * This is the latest source code of Vanilla Zoom.
+ * Minecraft version: 1.19.2, mod version: 1.0.
+ *
+ * Please don't distribute without permission.
+ * For all Minecraft modding projects, feel free to visit my profile page on CurseForge or Modrinth.
+ *  CurseForge: https://curseforge.com/members/serilum/projects
+ *  Modrinth: https://modrinth.com/user/serilum
+ *  Overview: https://serilum.com/
+ *
+ * If you are feeling generous and would like to support the development of the mods, you can!
+ *  https://ricksouth.com/donate contains all the information. <3
+ *
+ * Thanks for looking at the source code! Hope it's of some use to your project. Happy modding!
+ */
+
+package com.natamus.vanillazoom;
+
+import com.natamus.vanillazoom.events.ZoomEvent;
+import com.natamus.vanillazoom.util.Variables;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.event.player.UseEntityCallback;
+import net.fabricmc.fabric.api.event.player.UseItemCallback;
+import net.minecraft.client.KeyMapping;
+import net.minecraft.client.Minecraft;
+
+public class ClientMain implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		Variables.hotkey = KeyBindingHelper.registerKeyBinding(new KeyMapping("key.vanillazoom.togglezoom.desc", 342, "key.categories.misc"));
+
+		registerEvents();
+	}
+	
+	private void registerEvents() {
+		ClientTickEvents.START_CLIENT_TICK.register((Minecraft mc) -> {
+			ZoomEvent.onClientTick();
+		});
+
+		UseItemCallback.EVENT.register((player, world, hand) -> {
+			return ZoomEvent.onItemUse(player, world, hand);
+		});
+
+		UseEntityCallback.EVENT.register((player, world, hand, entity, hitResult) -> {
+			return ZoomEvent.onEntityInteract(player, world, hand, entity, hitResult);
+		});
+	}
+}
---
+++ sources-fabric/Vanilla Zoom (Fabric)/src/main/java/com/natamus/vanillazoom/events/ZoomEvent.java
@@ -0,0 +1,101 @@
+/*
+ * This is the latest source code of Vanilla Zoom.
+ * Minecraft version: 1.19.2, mod version: 1.0.
+ *
+ * Please don't distribute without permission.
+ * For all Minecraft modding projects, feel free to visit my profile page on CurseForge or Modrinth.
+ *  CurseForge: https://curseforge.com/members/serilum/projects
+ *  Modrinth: https://modrinth.com/user/serilum
+ *  Overview: https://serilum.com/
+ *
+ * If you are feeling generous and would like to support the development of the mods, you can!
+ *  https://ricksouth.com/donate contains all the information. <3
+ *
+ * Thanks for looking at the source code! Hope it's of some use to your project. Happy modding!
+ */
+
+package com.natamus.vanillazoom.events;
+
+import com.natamus.vanillazoom.util.Variables;
+import net.minecraft.client.KeyMapping;
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.SpyglassItem;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.EntityHitResult;
+
+public class ZoomEvent {
+    private static Minecraft mc = Minecraft.getInstance();
+	private static KeyMapping useKeyMapping = mc.options.keyUse;
+
+	public static boolean showZoom = false;
+	private static ItemStack previousStack = null;
+
+	public static void onClientTick() {
+		Player player = mc.player;
+		if (player == null) {
+			return;
+		}
+
+		if (!player.level.isClientSide) {
+			return;
+		}
+
+		boolean forceRelease = false;
+		if (showZoom) {
+			KeyMapping.set(useKeyMapping.getDefaultKey(), true);
+			KeyMapping.click(useKeyMapping.getDefaultKey());
+
+			if (!(player.getItemInHand(InteractionHand.OFF_HAND).getItem() instanceof SpyglassItem)) {
+				forceRelease = true;
+			}
+		}
+
+		if (Variables.hotkey.isDown() && !forceRelease) {
+			if (!showZoom) {
+				showZoom = true;
+
+				previousStack = player.getItemInHand(InteractionHand.OFF_HAND).copy();
+
+				ItemStack spyglassStack = new ItemStack(Items.SPYGLASS);
+				spyglassStack.setHoverName(Component.literal(""));
+				player.setItemInHand(InteractionHand.OFF_HAND, spyglassStack);
+			}
+		}
+		else {
+			if (showZoom) {
+				showZoom = false;
+				KeyMapping.set(useKeyMapping.getDefaultKey(), false);
+
+				if (previousStack != null) {
+					player.setItemInHand(InteractionHand.OFF_HAND, previousStack.copy());
+					previousStack = null;
+				}
+			}
+		}
+	}
+
+	public static InteractionResultHolder<ItemStack> onItemUse(Player player, Level level, InteractionHand hand) {
+		ItemStack itemStack = player.getItemInHand(hand);
+		if (showZoom) {
+			if (!(itemStack.getItem() instanceof SpyglassItem)) {
+				return InteractionResultHolder.fail(itemStack);
+			}
+		}
+		return InteractionResultHolder.pass(itemStack);
+	}
+
+	public static InteractionResult onEntityInteract(Player player, Level level, InteractionHand hand, Entity entity, EntityHitResult hitResult) {
+		if (showZoom) {
+			return InteractionResult.FAIL;
+		}
+		return InteractionResult.PASS;
+	}
+}
